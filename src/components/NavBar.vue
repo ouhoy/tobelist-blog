b0ae29e4f8d74de586056bbc26aaf76012543f4d
ouhoy <kimkinon@gmail.com>
2023-10-06 20:36:40 +0100

    Add responsive navbar and header

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,16 @@
 <script setup lang="ts">
 
+import {ref} from "vue";
+
+const showMenu = ref(false)
+
+function handleMenuClick() {
+
+  showMenu.value = !showMenu.value;
+
+
+}
+
 </script>
 
 <template>
@@ -7,15 +18,19 @@
   <div class="container">
     <div class="nav-container">
       <nav>
-
-
         <router-link :to="{name: 'landing'}">
 
           <div class="logo">
-            <img src='../assets/images/logo.svg' alt="Tobelist logo"/>
+
+            <img src="src/assets/icons/Logomark.svg" alt="tobelist logo">
+            <h4>tobelist.net</h4>
           </div>
         </router-link>
+
+
         <div class="menu-cta">
+
+
           <div class="menu hide-for-mobile hide-for-tablet">
             <router-link :to="{name: 'landing'}">
               Home
@@ -33,22 +48,43 @@
 
           <div class="cta">
 
-            <router-link :to="{name: 'login'}">
+            <router-link class="hide-for-mobile hide-for-tablet" :to="{name: 'login'}">
               Login
             </router-link>
             <router-link :to="{name: 'signup'}">
-              <button class="hide-for-mobile hide-for-tablet">
-                Get started
-              </button>
+              <button> Get started</button>
+            </router-link>
+            <div @click="handleMenuClick" class="ham-menu hide-for-desktop">
+              <img src="src/assets/icons/menu-icon.svg" alt="menu">
+            </div>
+
+          </div>
+          <div v-if="showMenu" class="mobile-menu">
+            <router-link :to="{name: 'landing'}">
+              Home
+            </router-link>
+            <a href="#">About</a>
+            <a href="#">Privacy</a>
+            <router-link :to="{name: 'blogs'}">
+              Blog
+            </router-link>
+            <router-link :to="{name: 'contact'}">
+              Contact
+            </router-link>
+
+            <router-link class="login" :to="{name: 'login'}">
+              Login
             </router-link>
           </div>
+
         </div>
+
       </nav>
     </div>
   </div>
 
 </template>
 
-<style scoped lang="scss">
+<style lang="scss">
 @import "../assets/styles/components/navbar";
 </style>
